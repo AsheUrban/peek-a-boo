6b7d265b45adcbab2e3f1d14045b61ff124b8fbc
Ashe Urban <ashe@goldentongue.com>
2022-06-01 11:13:32 -0700

    Reset to last functioning version.
    Co-authored-by: kylechinick <kylechinick@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-  $(".show").click(function() {
-    $("#walrus-showing").slideDown();
-  });
-});
-
-$(document).ready(function() {
-  $(".hide").click(function() {
-    $("#walrus-hidden").slideUp();
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
 });
